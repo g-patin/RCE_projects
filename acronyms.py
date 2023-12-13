f2843433ab1f3da5d177ffed4b78df117e16ef74
g-patin <85576027+g-patin@users.noreply.github.com>
2023-12-13 09:59:23 +0100

    Add files via upload

---
+++ acronyms.py
@@ -0,0 +1,375 @@
+######## ANALYSES ########
+
+analyses = {
+    'AIS': 'Absolute Irradiance Spectroscopy',
+    'GC-MS': 'Gas Chromatography - Mass Spectrometry',
+    'HPLC': 'High-Performance Liquid Chromatography',
+    'HSI': 'Hyperspectral Imaging',
+    'MFT': 'Microfading Testing',
+    'Photos': 'Photography',
+    'Power': 'Radiant power',
+    'Raman': 'Raman',
+    'RS': 'Reflectance Spectroscopy',
+    'SEM-EDX': 'Scanning Electron Microscopy - Energy Dispersive X-Ray',
+    'XRD': 'X-Ray Dispersion',
+    'XRF': 'X-Ray Fluorescence',
+}
+
+
+######## AREAS ########
+
+areas = {
+    'cc': 1.1946e-05
+}
+
+
+######## CAMERAS ########
+
+cameras = {
+    "IS1": "IS1_The imaging source DFK 41AU02",
+    "IS2": "IS2_The imaging source DFK 33UX183",
+    "ERc": "ERc_AxioCam ERc 5s Zeiss",
+}
+
+
+######## COLOURS ########
+
+colors = {
+    "b": "blue",
+    "br": "brown",
+    "g": "green",
+    "gy": "grey",
+    "k": "black",
+    "o": "orange",
+    "p": "pink",
+    "r": "red",
+    "v": "violet",
+    "w": "white",
+    "y": "yellow",
+}
+
+dE_colour = {
+    "dE76": "CIE 1976",
+    "dE00": "CIE 2000",    
+}
+
+
+######## DEVICES ########
+
+devices = {
+    "2201504U1": "Av_Avantes spectrometer, 2201504U1",
+    "i1": "i1_X-Rite i1 Pro",
+    "KM": "KM_Konica Minolta, ",
+    "Tidas": "Tidas_S 400",
+    "OO": "OO_Ocean Optics, USB4H06761",
+    "Mav": "Mav_Gossen, Mavospec base_19D10406",
+    "MFT-Fotonowy": "MFT-Fotonowy",
+    "PM100USB": "PM100USB_Thorlabs",
+    "stereo": "stereo_stereomicroscope, sMFT",
+    "sMFT": "stereo-MFT",
+}
+
+
+######## FIBERS ########
+
+fibers_size = {
+    "IR200um2": 200,
+    "UVIR200um2": 200,
+    "UVIR-200um-2": 200,
+    "400um0.5": 400,
+    "400um2": 400,
+    "UV400um0.5": 400,
+    "UV-400um-0.5": 400,
+    "UV-400um-2": 400,
+    "UVIR-400um-2": 400,
+    "UVIRb-400um-2-black": 400,
+    "UVIRb-400um-2-red": 400,
+    "600um1": 600,
+    "UV-600um-1": 600,
+    "UVIR600um1": 600,
+    "UVIR600um2": 600,
+    "UVIR-600um-2": 600,
+    "none": "none",
+}
+
+fibers = {
+    "IR-200um-2": "Avantes, IR200um-2m",
+    "IR200um2": "Avantes, IR200um-2m",
+    "UVIR-200um-2": "Avantes, FC-UVIR200-2-ME",
+    "400um0.5": "Avantes, UV400um-0.5m",
+    "UV-400um-0.5": "Avantes, UV400um-0.5m",
+    "400um2": "Avantes, UV400um-2m",
+    "UV-400um-2": "Avantes, UV400um-2m",
+    "600um1": "Avantes, UV600um-1m",
+    "UV-600um-1": "Avantes, UV600um-1m",
+    "UVIR-200um-2": "Avantes, UVIR200um-2m",
+    "UVIR-400um-2": "Avantes, FC-UVIR400-2-ME",
+    "UVIR400um2": "Avantes, FC-UVIR400-2-ME",
+    "UVIRb400um2-black": "Avantes, bifurcated UVIR-400um-2m-black dot leg",
+    "UVIRb400um2-red": "Avantes, bifurcated UVIR-400um-2m-red dot leg",
+    "UVIR-600um-2": "Avantes, FC-UVIR600-2-ME",
+    "UVIR600um2": "Avantes, FC-UVIR600-2-ME",
+    "UVIR-600um-1": "Avantes, FC-UVIR600-1-ME",
+    "UVIR600um1": "Avantes, FC-UVIR600-1-ME",
+    "none": "none",
+    "noFiber": "none",
+}
+
+
+######## FILTERS ########
+
+filters = {
+    "noFilter": "noFilter",
+    "VIS-25-01": "VIS-25-01_Linos Calflex-C heat protection",
+    "VIS-25-01+ND-22.4-05": "VIS-25-01+ND-22.4-05",
+    "VIS-25-01+ND-22.4-04": "VIS-25-01+ND-22.4-04",
+    "VIS-25-01+ND-22.4-03": "VIS-25-01+ND-22.4-03",
+    "VIS-25-01+ND-22.4-02": "VIS-25-01+ND-22.4-02",
+    "VIS-25-01+ND-25-01": "VIS-25-01+ND-25-01",
+    "VIS-25-01+ND-25-02": "VIS-25-01+ND-25-02",
+    "VIS-25-01+ND-25-03": "VIS-25-01+ND-25-03",
+    "VIS-25-01+ND-25-04": "VIS-25-01+ND-25-04",
+    "VIS-25-01+ND-25-05": "VIS-25-01+ND-25-05",
+    "ND-22.4-01": "ND-22.4-01",
+    "ND-22.4-02": "ND-22.4-02",
+    "ND-22.4-03": "ND-22.4-03",
+    "ND-22.4-04": "ND-22.4-04_Linos",
+    "ND-22.4-05": "ND-22.4-05_Linos G37 1142 000_1.0OD",
+    "ND-25-01": "ND-25-01_Edmund Optics #48089_0.5OD",
+    "ND-25-02": "ND-25-02_Edmund Optics #46212_0.3OD",
+    "ND-25-03": "ND-25-03_Edmund Optics #63460_0.1OD",
+    "ND-25-04": "ND-25-04_Edmund Optics #88270_0.3OD",
+    "ND-25-05": "ND-25-04_Edmund Optics #88272_1.0OD",
+}
+
+
+######## FOLDERS ########
+
+folder_data = {
+    "1.1": "/home/gus/Documents/PhD/PhD_Projects/Part1_MFT_Development/1.1_stereo-MFT/Data/",
+    "1.2": "/home/gus/Documents/PhD/PhD_Projects/Part1_MFT_Development/1.2_Dosimeter/Data/",
+    "1.3": "/home/gus/Documents/PhD/PhD_Projects/Part1_MFT_Development/1.3_Reciprocity-principle/Data/",
+    "1.4": "/home/gus/Documents/PhD/PhD_Projects/Part1_MFT_Development/1.4_Temperature-surface/Data/",
+    "2.1": "/home/gus/Documents/PhD/PhD_Projects/Part2_Paint_&_colour_technology/2.1_Model-paint-outs/Data/",
+    "2.2": "/home/gus/Documents/PhD/PhD_Projects/Part2_Paint_&_colour_technology/2.2_Light-box-exp/Data/",
+    "2.4": "/home/gus/Documents/PhD/PhD_Projects/Part2_Paint_&_colour_technology/2.4_Sunflowers-project/Colour_monitoring/Data/",
+    "2.6": "/home/gus/Documents/PhD/PhD_Projects/Part2_Paint_&_colour_technology/2.6_Window-exp/Data/",
+    "3.3": "/home/gus/Documents/PhD/PhD_Projects/Part3_MFT_Application/3.3_PO-analysis/Data/",
+    "3.4.4": "/home/gus/Documents/PhD/PhD_Projects/Part3_MFT_Application/3.4_MFT-external/3.4.4_SOP_Rika/Data/",
+    "3.4.5": "/home/gus/Documents/PhD/PhD_Projects/Part3_MFT_Application/3.4_MFT-external/3.4.5_Indian-paper_Amelie/Data/",
+    "3.4.6": "/home/gus/Documents/PhD/PhD_Projects/Part3_MFT_Application/3.4_MFT-external/3.4.6_Cyclorama_RMA/Data/",
+    "3.4.7": "/home/gus/Documents/PhD/PhD_Projects/Part3_MFT_Application/3.4_MFT-external/3.4.7_RP_Rahul/Data/",
+    "3.4.8": "/home/gus/Documents/PhD/PhD_Projects/Part3_MFT_Application/3.4_MFT-external/3.4.8_Ilfochrome_Hofmann/Data/",
+    "3.4.9": "/home/gus/Documents/PhD/PhD_Projects/Part3_MFT_Application/3.4_MFT-external/3.4.9_Inktime_Frank/inktime/data/",
+    "3.5": "/home/gus/Documents/PhD/PhD_Projects/Part3_MFT_Application/3.5_Fading-depth/Data/",
+    "3.6": '/home/gus/Documents/PhD/PhD_Projects/Part3_MFT_Application/3.6_Fading-comparison/Data/',
+    "3.7": "/home/gus/Documents/PhD/PhD_Projects/Part3_MFT_Application/3.7_Sunflowers-rec/Data/",
+    "3.8":  "/home/gus/Documents/PhD/PhD_Projects/Part3_MFT_Application/3.8_Colour-reversion/Data/",
+}
+
+folder_figures = {
+    "1.1": "/home/gus/Documents/PhD/PhD_Projects/Part1_MFT_Development/1.1_stereo-MFT/Figures/",
+    "1.2": "/home/gus/Documents/PhD/PhD_Projects/Part1_MFT_Development/1.2_Dosimeter/Figures/",
+    "1.3": "/home/gus/Documents/PhD/PhD_Projects/Part1_MFT_Development/1.3_Reciprocity-principle/Figures/",
+    "1.4": "/home/gus/Documents/PhD/PhD_Projects/Part1_MFT_Development/1.4_Temperature-surface/Figures/",
+    "2.1": "/home/gus/Documents/PhD/PhD_Projects/Part2_Paint_&_colour_technology/2.1_Model-paint-outs/Figures/",
+    "2.2": "/home/gus/Documents/PhD/PhD_Projects/Part2_Paint_&_colour_technology/2.2_Light-box-exp/Figures/",
+    "2.6": "/home/gus/Documents/PhD/PhD_Projects/Part2_Paint_&_colour_technology/2.6_Window-exp/Figures/",
+    "3.3": "/home/gus/Documents/PhD/PhD_Projects/Part3_MFT_Application/3.3_PO-analysis/Figures/",
+    "3.4.4": "/home/gus/Documents/PhD/PhD_Projects/Part3_MFT_Application/3.4_MFT-external/3.4.4_SOP_Rika/Figures/",
+    "3.4.5": "/home/gus/Documents/PhD/PhD_Projects/Part3_MFT_Application/3.4_MFT-external/3.4.5_Indian-paper_Amelie/Figures/",
+    "3.4.8": "/home/gus/Documents/PhD/PhD_Projects/Part3_MFT_Application/3.4_MFT-external/3.4.8_Ilfochrome_Hofmann/Figures/",
+    "3.4.9": "/home/gus/Documents/PhD/PhD_Projects/Part3_MFT_Application/3.4_MFT-external/3.4.9_Inktime_Frank/inktime/figures",
+    "3.5": "/home/gus/Documents/PhD/PhD_Projects/Part3_MFT_Application/3.5_Fading-depth/Figures/",
+    "3.6": '/home/gus/Documents/PhD/PhD_Projects/Part3_MFT_Application/3.6_Fading-comparison/Figures/',
+    "3.7": "/home/gus/Documents/PhD/PhD_Projects/Part3_MFT_Application/3.7_Sunflowers-rec/Figures/",
+    "3.8":  "/home/gus/Documents/PhD/PhD_Projects/Part3_MFT_Application/3.8_Colour-reversion/Figures/",
+}
+
+folder_types = {
+    "ill": "Ill",
+    "irr": "Irr",
+    "I": "AIS",
+    "MF": "MFT",
+    "P": "Power",
+    "RH": "RH",
+    "RS": "RS",
+    "T": "Temp",
+    "TS": "TS",
+}
+
+
+######## GEOMETRIES ########
+
+geometries = {
+    "AS30": "AvaSphere 30 - 8:d",
+    "AS80": "AvaSphere 80 - 8:d",    
+    "stereo": "45° : 0°",
+    "E.1.1": "45° : 0°",    
+    "E.2.1": "0° : 45°",
+    "E.3.1": "45° : 0°",
+    "KM": "di:8.de:8",
+    "i1": "45° : 0°",
+    "COA": "0°:(45°:0°)"
+}
+
+
+######## INSTITUTIONS ########
+
+institutions = {
+    'DNB' : 'De Nederlandsche Bank',
+    'MBVB' : 'Museum Boijmans van Beuningen',
+    'OdK-Adm' : 'Oude Kerk Amsterdam',
+    'PETRACON' : 'PETRACON',
+    'REL' : 'Rijkserfgoed Laboratorium',
+    'RCE' : 'Rijksdienst voor het Cultureel Erfgoed',    
+    'RMA' : 'Rijksmuseum Amsterdam',
+    'SOGK' : 'Stichting Oude Groninger Kerken',
+    'VGM' : 'Van Gogh Museum',    
+    'UvA' : 'University of Amsterdam',
+}
+
+
+######## LAMPS ########
+
+lamps = {
+    "daylight": "daylight",
+    "HAL1": "HAL1_Avantes, halogen",
+    "HAL2": "HAL2_",
+    "HAL3": "HAL3_Ocean Optics, HL-2000-FHSA-LL",
+    "HAL4": "HAL4_",
+    "HPX1": "HPX1_Ocean Optics, HPX-2000-HP-DUV",
+    "HPX2": "HPX2_Ocean Optics, HPX-2000-HP-DUV",
+    "KM" : "Xe_3 pulsed xenon lamps",
+    "LED1": "LED1_cold white, single ring",
+    "LED2": "LED2_cold white, double ring",
+    "LED3": "LED3_Tidas S 400, back illumination, cold white",
+    "LED4": "LED4_Xicato, XTM.95 3000K warm white (VGM)",
+    "LED5": "LED5_Thorlabs, MWWHF2",
+    "i1": "TypeA_gas filled tungsten",
+    "Xe_UV": "Xe_3 pulsed xenon lamps with UV",    
+    "Xe_UVcut": "Xe_3 pulsed xenon lamps UV cut",
+    "400nm cut": "Xe_3 pulsed xenon lamps UV cut",
+    "none": "none",
+    "100 Full": "Xe_3 pulsed xenon lamps with UV",
+}
+
+
+######## MEDEWERKERS ########
+
+medewerkers = {
+    'none' : 'none',
+    'AB' : 'Agnes Brokerhof',
+    'BA' : 'Bart Ankersmit',
+    'BR' : 'Birgit Reissland',
+    'EvR': 'Elsemieke van Rietschoten',
+    'FL' : 'Frank Ligterink',
+    'GP' : 'Gauthier Patin',
+    'KJvdB' : 'Klaas Jan van den Berg',
+    'IJ' : 'Ineke Joosten',
+    'MC' : 'Mariska Castelijn',
+    'MG' : 'Muriel Geldof',
+    'OvR' : 'Olivia van Rooijen',
+    'RM' : 'Rutger Morelissen',
+    'RP' : 'Rika Pause',
+    'SB' : 'Sanne Berbers',
+    'SdG' : 'Suzan de Groot'
+}
+
+
+######## MEASUREMENT MODES ########
+
+modes = {
+    "COA": 'COA_stereo-MFT Co-axial mode',
+    "cc": 'cc_Ocean Optics cosine corrector',
+    "bfw": 'bfw_bare fiber',
+    "LampSide" : "Lamp on the side"
+}
+
+
+######## COLORANTS ########
+
+colorants = {
+    "BS381C": "unknown",
+    "BW1": "Acid-blue104",
+    "BW2": "Acid-blue109",
+    "BW3": "Acid-blue83",
+    "BW4": "Acid-blue121",
+    "C.I. Acid Blue 104": "Acid-blue104",
+    "C.I. Acid Blue 109": "Acid-blue109",
+    "C.I. Acid Blue 83": "Acid-blue83",
+    "C.I. Acid Blue 121": "Acid-blue121",
+
+}
+
+positions = {
+    "V": "V_vertical",
+    "H": "H_horizontal",
+}
+
+
+scs = {
+    "E.1.1": "included",    
+    "E.2.1": "included",
+    "E.3.1": "included",
+
+}
+
+sensor_areas = {
+    "cc": 1.1946e-05,
+    "bfw": "bare fiber",
+    "Mav": 0.000038485,
+    "S405C": 0.0001,
+}
+
+sensor_types = {
+    "cc": "cc_cosine corrector Ocean Optics",
+    "bfw": "bare fiber",
+    "Mav": "CMOS_image sensor 256 pixels",
+    "S405C": "S405C_Thorlabs thermopile-based sensor",
+}
+
+settings = {
+    "2.2": "box2.1",
+    "2.6": "window",
+}
+
+supports = {
+    "BS381C": "white paper",
+    "BW": "wool",
+    "PO": "Black and white Leneta chart",
+    "X-riteCC-SGcard": "unknown",
+    "X-riteCC-classic": "unknown",
+}
+
+systems = {
+    "KM": "KM_Konica Minolta spectrophotometer",
+    "stereo": "stereo_stereomicroscope stemi SV11 Zeiss"
+}
+
+unit_dE = {
+    'dE76': '$\Delta E^*_{ab}$',
+    'dE00': '$\Delta E^*_{00}$',
+    'dR_VIS': r'$\Delta R_{\rm vis}$'
+}
+
+thicknesses = {
+    "BS381C": "unknown",
+    "BW": "not applicable",
+    "PO": 100,
+    "X-riteCC-SGcard": "unknown",
+    "X-riteCC-classic": "unknown",
+    "ceramic-tile": 6000,
+}
+
+WRs = {
+    "cc": "cc_Ocean Optics, cosine corrector",
+    "WR1": "WR1_Dr Lange, pressed barium sulphate powder",
+    "WR2": "WR2_Konica Minolta, calibration plate 22191004",
+    "WR3": "WR3_Konica Minolta, CM-A145",
+    "PO003": "PO003, ivory black",    
+}
+
+
+
+
